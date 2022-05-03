7707dbdf0bf319a213c666d76b09b2e9e554f91d
kylin <40333954+InfernalAzazel@users.noreply.github.com>
2022-05-03 14:15:48 +0800

    新增 tab

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,33 +1,33 @@
 <template>
-  <pro-layout
-      :locale="locale"
-      v-bind="layoutConf"
-      v-model:openKeys="state.openKeys"
-      v-model:collapsed="state.collapsed"
-      v-model:selectedKeys="state.selectedKeys"
-      :breadcrumb="{ routes: breadcrumb }"
+    <pro-layout
+        :locale="locale"
+        v-bind="layoutConf"
+        v-model:openKeys="state.openKeys"
+        v-model:collapsed="state.collapsed"
+        v-model:selectedKeys="state.selectedKeys"
+        :breadcrumb="{ routes: breadcrumb }"
 
-  >
-    <template #headerContentRender>
-      <a :style="{ margin: '0 8px', fontSize: '20px' }" @click="handleCollapsed">
-        <MenuUnfoldOutlined v-if="state.collapsed"/>
-        <MenuFoldOutlined v-else/>
-      </a>
-    </template>
-    <!-- custom right-content -->
-    <template #rightContentRender>
-      <Notice :count="0"/>
-      <AvatarList name="kylin"/>
-      <LangList/>
-    </template>
-    <template #footerRender>
-      <div>
-        <h5 class="flex justify-center text-yellow-500">Copyright © 2022 kylin</h5>
-      </div>
-    </template>
-    <router-view></router-view>
+    >
+        <!--    <template #headerContentRender>-->
+        <!--      <a :style="{ margin: '0 8px', fontSize: '20px' }" @click="handleCollapsed">-->
+        <!--        <MenuUnfoldOutlined v-if="state.collapsed"/>-->
+        <!--        <MenuFoldOutlined v-else/>-->
+        <!--      </a>-->
+        <!--    </template>-->
+        <!-- custom right-content -->
+        <!--    <template #rightContentRender>-->
+        <!--      <Notice :count="0"/>-->
+        <!--      <AvatarList name="kylin"/>-->
+        <!--      <LangList/>-->
+        <!--    </template>-->
+        <template #footerRender>
+            <div>
+                <h5 class="flex justify-center text-yellow-500">Copyright © 2022 kylin</h5>
+            </div>
+        </template>
+        <router-view></router-view>
 
-  </pro-layout>
+    </pro-layout>
 </template>
 
 
@@ -45,28 +45,29 @@ const loading = ref(false);
 const {menuData} = getMenuData(clearMenuItem(router.getRoutes()));
 
 const handleCollapsed = () => {
-  state.collapsed = !state.collapsed;
+    state.collapsed = !state.collapsed;
 };
 const breadcrumb = computed(() =>
     router.currentRoute.value.matched.concat().map(item => {
-      return {
-        path: item.path,
-        breadcrumbName: item.meta.title || '',
-      };
+        return {
+            path: item.path,
+            breadcrumbName: item.meta.title || '',
+        };
     }),
 );
 const state = reactive({
-  collapsed: false,
-  openKeys: [],
-  selectedKeys: [],
+    collapsed: false,
+    openKeys: [],
+    selectedKeys: [],
 })
 const layoutConf = reactive({
-  title: 'AdminTemplate',
-  navTheme: 'light',
-  layout: 'side',
-  splitMenus: false,
-  menuData,
-  fixSiderbar: true,
-  // fixedHeader: true,
+    title: 'AdminTemplate',
+    navTheme: 'light',
+    layout: 'side',
+    splitMenus: false,
+    menuData,
+    headerHeight: 48,
+    //fixSiderbar: true,
+     fixedHeader: true,
 });
 </script>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,32 @@
 <template>
-  <page-container :title="`${$route.meta.title}`" sub-title="is a sub-title.">
-    <template #tags>
-      <a-tag>tag1</a-tag>
-      <a-tag color="pink">tag2</a-tag>
-    </template>
-    <a-button @click="handleClick">Button</a-button>
-    <p v-for="item in new Array(20)" :key="item">text block...</p>
+    <page-container
+        :tab-list="tab.list"
+        :tab-active-key="tab.activeKey"
+        :on-tab-change="handleTabChange">
+            <template #tags>
+              <a-tag>tag1</a-tag>
+              <a-tag color="pink">tag2</a-tag>
+            </template>
+            <a-button @click="handleClick">Button</a-button>
+            <p v-for="item in new Array(20)" :key="item">text block...</p>
 
-  </page-container>
+    </page-container>
 </template>
+<script lang="ts" setup>
+import {reactive} from "vue";
+
+const tab = reactive({
+    list: [
+        {tab: 'tab1', key: 'tab1'},
+        {tab: 'tab2', key: 'tab2'},
+        {tab: 'tab3', key: 'tab3'}
+    ],
+    activeKey : 'tab1'
+})
+
+const handleTabChange = (key: any) => {
+    tab.activeKey = key
+    console.log('PageHeader::tabChange', key)
+}
+</script>
 <style></style>
